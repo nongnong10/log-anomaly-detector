c6287243e4851c4a8a332d950804966b5c2089b2
huy.nkq <jirachihuy@gmail.com>
2025-11-24 06:42:09 +0700

    [update] Update CORS origins

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ app = FastAPI(title="Log Anomaly Detection API")
 
 # CORS middleware (adjust origins as needed)
 origins = [
-    "http://localhost:8080",  # Vite dev server
-    "http://127.0.0.1:8080"
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
